078f28cd63f26733e9bda5f6560a39a8b363a309
Mohammad Asad Rahmani <146559979+rahmani3101@users.noreply.github.com>
2024-12-13 17:24:12 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World ! ");
+        System.out.println("Dockerfile test in java \n Name : Mohammad Asad Rahmani ");
+    }
+}
